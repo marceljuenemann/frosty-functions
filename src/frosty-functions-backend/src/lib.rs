9b690293a40e4071f3bc337cdafae1d5c6a312ce
Marcel Juenemann <mail@marcel-juenemann.de>
2025-10-06 21:36:51 +0100

    Integrate wasm runtime

--- src/frosty-functions-backend/src/lib.rs
+++ src/frosty-functions-backend/src/lib.rs
@@ -1,3 +1,31 @@
+use wasmi::*;
+
+fn run_wasm() -> Result<i64, String> {
+    let wasm = include_bytes!("../../../../ic-wasmi-benchmark/target/wasm32-unknown-unknown/wasm/bench.wasm");
+    let engine = Engine::default();
+    let module = Module::new(&engine, &wasm[..]).unwrap();
+    let linker = <wasmi::Linker<()>>::new(module.engine());
+    let mut store = wasmi::Store::new(module.engine(), ());
+    let instance = linker
+        .instantiate(&mut store, &module)
+        .unwrap()
+        .start(&mut store)
+        .unwrap();
+
+    let run = instance.get_typed_func::<(), i64>(&store, "run").unwrap();
+    Ok(run.call(&mut store, ()).unwrap())
+}
+
+#[ic_cdk::query]
+fn runWasm() -> String {
+    let sum = run_wasm().unwrap();
+    format!(
+        "instructions: {}, sum: {}",
+        ic_cdk::api::performance_counter(0),
+        sum
+    )
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
