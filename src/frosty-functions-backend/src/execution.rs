724c2ddf2e3cb901eee6b1c1d36f5eb68e66c463
Marcel Juenemann <mail@marcel-juenemann.de>
2025-11-19 22:25:04 +0000

    Create JobExecution object in preparation for async handling

--- src/frosty-functions-backend/src/execution.rs
+++ src/frosty-functions-backend/src/execution.rs
@@ -1,6 +1,8 @@
-use wasmi::{Engine, Func, Linker, Module, Store, core::TrapCode, errors::{ErrorKind, FuelError}};
+use std::collections::VecDeque;
 
-use crate::{job::JobRequest, state::read_chain_state};
+use wasmi::{Engine, Func, Linker, Module, Store, core::TrapCode};
+
+use crate::{job::{Job, JobRequest}, state::read_chain_state};
 
 const FUEL_PER_BATCH: u64 = 1_000_000;
 
@@ -14,53 +16,74 @@ pub async fn execute_job(chain_id: String, job_id: u64) -> Result<(), String> {
     
     // TODO: Verify status is "pending" and set to "in progress".
 
-    execute_binary(&request, binary).await
+    let mut execution = JobExecution::init(request.clone(), binary)?;
+    execution.call("main".to_string()).await
 }
 
-// TODO: Return an Execution error that can be logged.
-async fn execute_binary(request: &JobRequest, wasm: &[u8]) -> Result<(), String> {
-    let mut config = wasmi::Config::default();
-    config.consume_fuel(true);
-    let engine = Engine::new(&config);
+/// Runtime state for a job execution.
+pub struct JobExecution {
+    pub request: JobRequest,
+    pub store: wasmi::Store<()>,
+    pub instance: wasmi::Instance,
+    // TODO: Keep a queue of callbacks to invoke and futures to poll.
+}
 
-    let module = Module::new(&engine, &wasm[..]).map_err(|e| format!("Failed to load WASM module: {}", e))?;
-    let mut store = wasmi::Store::new(module.engine(), ());
-    // TODO: Set instruction limit (fuel) based on available gas.
-    store.set_fuel(FUEL_PER_BATCH).map_err(|e| format!("Failed to set fuel: {}", e))?;
+impl JobExecution {
+    /// Creates a wasmi Engine and initializes the module. 
+    pub fn init(request: JobRequest, wasm: &[u8]) -> Result<Self, String> {
+        let mut config = wasmi::Config::default();
+        config.consume_fuel(true);
+        let engine = Engine::new(&config);
 
-    let mut linker = <wasmi::Linker<()>>::new(module.engine());
-    register_host_functions(&mut linker, &mut store)?;
+        let module = Module::new(&engine, &wasm[..]).map_err(|e| format!("Failed to load WASM module: {}", e))?;
+        let mut store = wasmi::Store::new(module.engine(), ());
+        // TODO: Set instruction limit (fuel) based on available gas.
+        store.set_fuel(FUEL_PER_BATCH).map_err(|e| format!("Failed to set fuel: {}", e))?;
 
-    // TODO: Replace ic_cdk::println with custom logging to the job logs.
-    ic_cdk::println!("Executing job: {:?}", request.on_chain_id);
-    let instance = linker.instantiate(&mut store, &module)
-        .map_err(|e| format!("Failed to instantiate WASM module: {}", e))?
-        .start(&mut store)
-        // TODO: Might want to handle out of fuel errors differently here.
-        .map_err(|e| format!("Failed to start WASM module: {}", e))?;
+        let mut linker = <wasmi::Linker<()>>::new(module.engine());
+        register_host_functions(&mut linker, &mut store)?;
 
-        ic_cdk::println!("Executing WASM function...");
+        // TODO: Replace ic_cdk::println with custom logging to the job logs.
+        ic_cdk::println!("Executing job: {:?}", request.on_chain_id);
+        let instance = linker.instantiate(&mut store, &module)
+            .map_err(|e| format!("Failed to instantiate WASM module: {}", e))?
+            // TODO: Allow fail if async host functions are called during initialization?
+            .start(&mut store)
+            // TODO: Might want to handle out of fuel errors differently here.
+            .map_err(|e| format!("Failed to start WASM module: {}", e))?;
     
-    // We interrupt and resume execution based on fuel consumption.
-    let entry_point = instance.get_typed_func::<(), ()>(&store, "main")
-        .map_err(|e| format!("Failed to find main() function: {}", e))?;
-    loop {
-        match entry_point.call(&mut store, ()) {
-            Ok(()) => {
-                // Execution completed successfully
-                let remaining_fuel = store.get_fuel().unwrap_or(0);
-                let fuel_consumed = FUEL_PER_BATCH - remaining_fuel;
-                ic_cdk::println!("Execution completed. Fuel consumed: {:?}", fuel_consumed);
-                return Ok(());
-            }
-            Err(e) => {
-                if let Some(TrapCode::OutOfFuel) = e.as_trap_code() {
-                    // TODO: Deduct cycles from gas, then resume execution if more gas is available.
-                    // store.set_fuel(FUEL_PER_BATCH).map_err(|e| format!("Failed to refuel: {}", e))?;
-                    return Err("WASM execution ran out of fuel".to_string());
+        Ok(Self {
+            request,
+            store,
+            instance,
+        })
+    }
+
+    /// Calls a function of the WASM module.
+    async fn call(&mut self, function_name: String) -> Result<(), String> {
+        ic_cdk::println!("Executing WASM function {:?}...", function_name);
+
+        // We interrupt and resume execution based on fuel consumption.
+        let func = self.instance.get_typed_func::<(), ()>(&self.store, &function_name)
+            .map_err(|e| format!("Failed to get function with name: {:?}", function_name))?;
+        loop {
+            match func.call(&mut self.store, ()) {
+                Ok(()) => {
+                    // Execution completed successfully
+                    let remaining_fuel = self.store.get_fuel().unwrap_or(0);
+                    let fuel_consumed = FUEL_PER_BATCH - remaining_fuel;
+                    ic_cdk::println!("Execution completed. Fuel consumed: {:?}", fuel_consumed);
+                    return Ok(());
                 }
-                // Other execution error (trap, validation, etc.)
-                return Err(format!("WASM execution failed: {}", e));
+                Err(e) => {
+                    if let Some(TrapCode::OutOfFuel) = e.as_trap_code() {
+                        // TODO: Deduct cycles from gas, then resume execution if more gas is available.
+                        // store.set_fuel(FUEL_PER_BATCH).map_err(|e| format!("Failed to refuel: {}", e))?;
+                        return Err("WASM execution ran out of fuel".to_string());
+                    }
+                    // Other execution error (trap, validation, etc.)
+                    return Err(format!("WASM execution failed: {}", e));
+                }
             }
         }
     }
@@ -72,7 +95,7 @@ fn register_host_functions(linker: &mut Linker<()>, store: &mut Store<()>) -> Re
         .map_err(|e| format!("Failed to define example_host_function: {}", e))?;
     Ok(())
 }
-    
+
 fn example_host_function() -> i64 {
     ic_cdk::println!("example_host_function invoked");
     ic_cdk::api::time() as i64
