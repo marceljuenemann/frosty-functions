adccb8c4ac197ec9d0dd87a51391179847b87668
Marcel Juenemann <mail@marcel-juenemann.de>
2025-11-25 18:20:11 +0000

    Install bootstrap into angular frontend

--- src/frosty-ng/src/app/app.ts
+++ src/frosty-ng/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbAlert, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbAlert],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
