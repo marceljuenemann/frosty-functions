35ceeb49d86d36701c0820c452c8d909fb78dc30
Marcel Juenemann <mail@marcel-juenemann.de>
2025-11-25 19:30:11 +0000

    Configure Monaco editor more

--- src/frosty-ng/src/app/monaco-editor/monaco-editor.ts
+++ src/frosty-ng/src/app/monaco-editor/monaco-editor.ts
@@ -9,8 +9,17 @@ import { MonacoEditorModule, NgxEditorModel } from 'ngx-monaco-editor-v2';
   styleUrl: './monaco-editor.scss',
 })
 export class MonacoEditor {
-  editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  editorOptions = {
+    theme: 'vs-dark',
+    language: 'typescript',
+    automaticLayout: true,
+    tabSize: 2,
+    padding: {
+      bottom: 18,
+      top: 18
+    }
+  };
+  code: string= 'function x() {\n  console.log("Hello world!");\n}';
   model: NgxEditorModel = {
     value: this.code,
     language: 'typescript',
