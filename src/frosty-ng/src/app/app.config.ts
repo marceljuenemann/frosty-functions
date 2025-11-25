89263afab6d72af86dde3467069f750d7e6ef99d
Marcel Juenemann <mail@marcel-juenemann.de>
2025-11-25 18:56:52 +0000

    First steps for Monaco editor

--- src/frosty-ng/src/app/app.config.ts
+++ src/frosty-ng/src/app/app.config.ts
@@ -1,12 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { NGX_MONACO_EDITOR_CONFIG, NgxMonacoEditorConfig } from 'ngx-monaco-editor-v2';
 
 import { routes } from './app.routes';
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  // baseUrl: '/assets/monaco/min/vs',
+  defaultOptions: { scrollBeyondLastLine: false }
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: NGX_MONACO_EDITOR_CONFIG, useValue: monacoConfig }
   ]
 };
--- src/frosty-ng/src/app/monaco-editor/monaco-editor.ts
+++ src/frosty-ng/src/app/monaco-editor/monaco-editor.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Uri } from 'monaco-editor';
+import { MonacoEditorModule, NgxEditorModel } from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'monaco-editor',
-  imports: [],
+  imports: [MonacoEditorModule],
   templateUrl: './monaco-editor.html',
   styleUrl: './monaco-editor.scss',
 })
 export class MonacoEditor {
-
+  editorOptions = {theme: 'vs-dark', language: 'javascript'};
+  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  model: NgxEditorModel = {
+    value: this.code,
+    language: 'typescript',
+    uri: Uri.parse('file://function.ts')
+  };
 }
