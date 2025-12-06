3256a189f601365bcc3a7b0686db889bc78f99fe
Marcel Juenemann <mail@marcel-juenemann.de>
2025-12-06 18:42:55 +0000

    UI for transaction submission

--- src/frosty-ng/src/app/invoke-function/invoke-function.ts
+++ src/frosty-ng/src/app/invoke-function/invoke-function.ts
@@ -1,6 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { FunctionState } from 'declarations/frosty-functions-backend/frosty-functions-backend.did';
 import { SignerService } from '../signer';
+import { TransactionReceipt, TransactionResponse } from 'ethers';
+
+// TODO: Configurable
+const CHAIN_ID = 31337;
+// const chainId = 421614;
+
 
 @Component({
   selector: 'app-invoke-function',
@@ -11,17 +17,59 @@ import { SignerService } from '../signer';
 export class InvokeFunctionComponent {
   function = input.required<FunctionState>();
 
+  // There's three UI states for each step: null (not started), 'pending', and a value (completed).
+  transactionId = signal<'pending' | string | null>(null);
+  blockNumber = signal<'pending' | string | null>(null);
+  jobId = signal<'pending' | number | null>(null);
+  error = signal<string | null>(null);
+
+  // TODO: Set based on chain ID
+  scannerUrl = signal<string | null>('https://sepolia.arbiscan.io');
+
   constructor(private signerService: SignerService) {}
 
   async runFunction() {
-    //const chainId = 31337;
-    const chainId = 421614;
-    const calldata = new Uint8Array([]);  // TODO: configure
-    const amount = BigInt(123456);
+    this.transactionId.set(null);
+    this.blockNumber.set(null);
+    this.jobId.set(null);
+    this.error.set(null);
 
-    const tx = await this.signerService.invokeFrostyFunction(chainId, this.function(), calldata, amount);
-    const receipt = await tx.wait();
-    console.log('Receipt:', receipt);
-    // TODO: Update UI
+    const tx = await this.submitTransaction();
+    const receipt = await this.waitForBlockInclusion(tx);
+
   }
+
+  private async submitTransaction(): Promise<TransactionResponse> {
+    this.transactionId.set('pending');
+    try {
+      const calldata = new Uint8Array([]);  // TODO: configure
+      const amount = BigInt(123456);
+      const tx = await this.signerService.invokeFrostyFunction(CHAIN_ID, this.function(), calldata, amount);
+      this.transactionId.set(tx.hash);
+      return tx;
+    } catch (e) {
+      this.error.set(`Transaction submission failed: ${e}`);
+      this.transactionId.set(null);
+      throw e;
+    }
+  }
+
+  private async waitForBlockInclusion(tx: TransactionResponse): Promise<TransactionReceipt> {
+    this.blockNumber.set('pending');
+    try {
+      const receipt = await tx.wait();
+      if (receipt == null) {
+        this.error.set('Transaction failed: no receipt');
+        this.blockNumber.set(null);
+        throw new Error("No receipt");
+      }
+      this.blockNumber.set(receipt.blockNumber.toString());
+      return receipt;
+    } catch (e) {
+      this.error.set(`Waiting for block inclusion failed: ${e}`);
+      this.blockNumber.set(null);
+      throw e;
+    }
+  }
+
 }
